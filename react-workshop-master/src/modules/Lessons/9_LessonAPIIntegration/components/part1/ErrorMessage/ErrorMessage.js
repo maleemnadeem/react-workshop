8d99348bda7c810b62ecaac39917aa1ba4d99ed0
maleemnadeem <aleemchougtai@gmail.com>
2018-10-15 22:55:00 +0500

    add new files

---
+++ react-workshop-master/src/modules/Lessons/9_LessonAPIIntegration/components/part1/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropType from 'prop-types'
+
+const ErrorMessage = ({error})=>{
+
+    return(
+            <div>
+                <h1>OOPS......</h1>
+                <span className=" message">{error.toString()}</span>
+            </div>
+    );
+};
