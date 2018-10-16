5e1dd7b7158cb93b40d672bfd532899d35d61018
aleem <muhammad.aleem@arbisoft.com>
2018-10-16 17:12:58 +0500

    updated

--- react-workshop-master/src/modules/Lessons/9_LessonAPIIntegration/components/part1/GitDashboard.js
+++ react-workshop-master/src/modules/Lessons/9_LessonAPIIntegration/components/part1/GitDashboard.js
@@ -1,4 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
+import ReactLoader from 'react-loading';
+import axios from 'axios';
+import ErrorMessage from './ErrorMessage/ErrorMessage'
+import UserCard from './UserCard/UserCard'
+import SearchBox from './SearchBox/SearchBox'
+
 
 class GitDashboard extends Component {
   constructor(props) {
@@ -6,12 +12,75 @@ class GitDashboard extends Component {
 
     this.state = {
       data: null,
+        isLoading:true,
+        errorLoading:false,
+        searchQuery:'',
     };
+    this.loadData=this.loadData.bind(this);
+    this.onQueryTextChange=this.onQueryTextChange.bind(this);
+  }
+    componentWillMount() {
+        this.loadData();
+    }
+
+  loadData(){
+    axios.get('https://api.github.com/users')
+        .then(({data})=>{
+          this.setState(
+              {
+                  isLoading:!this.state.isLoading,
+                  data,
+              }
+          );
+        }).catch(response => {
+          console.log('Data is not loading',response) ;
+          this.setState({
+              isLoading:!this.state.isLoading,
+              errorLoading:response,
+          });
+        }
+
+    );
+  }
+
+  onQueryTextChange(event){
+        this.setState(
+            {
+                searchQuery:event.target.value,
+            }
+        );
   }
 
   render() {
+      const { isLoading, errorLoading, data ,searchQuery} = this.state;
     return (
-        <h4>Git Dashboard</h4>
+        <div>
+          <h4>Git Dashboard</h4>
+          {
+             isLoading && <ReactLoader color="#000000" type="spin" className="loader" />
+          }
+          {
+             errorLoading && <ErrorMessage error={errorLoading}/>
+          }
+          {
+              !isLoading && !errorLoading &&
+              (
+                  <Fragment>
+                    <SearchBox isQuery={searchQuery} onChangeText={this.onQueryTextChange}/>
+                      {
+                          searchQuery && data.filter((user) => user.login.indexOf(searchQuery)> -1)
+                              .map((user) => <UserCard key={user.id} user={user}/>)
+                      }
+                      {
+                          !searchQuery && data.map((user) => <UserCard key={user.id} user={user}/>)
+                      }
+                  </Fragment>
+              )
+
+          }
+
+
+      </div>
     );
   }
 }
