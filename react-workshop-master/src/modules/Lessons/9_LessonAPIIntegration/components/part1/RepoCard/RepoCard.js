27978f00bbb9b3fa1254bad6d7d5f47da61d78b3
maleemnadeem <aleemchougtai@gmail.com>
2018-10-16 23:38:14 +0500

    updated

--- react-workshop-master/src/modules/Lessons/9_LessonAPIIntegration/components/part1/RepoCard/RepoCard.js
+++ react-workshop-master/src/modules/Lessons/9_LessonAPIIntegration/components/part1/RepoCard/RepoCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropsType from 'prop-types'
+import PropTypes from 'prop-types';
 
 const RepoCard = (repo)=>{
 
@@ -15,14 +15,47 @@ const RepoCard = (repo)=>{
                         <td>
                             {repo.description ? repo.description : 'N/A'}
                         </td>
+                    </tr>
                         <tr>
                             <strong><td>Defult Branch</td></strong>
-                            <td>repo.branch</td>
+                            <td>{repo.branch}</td>
                         </tr>
-                    </tr>
+                        <tr>
+                            <td>
+                                <strong>Language</strong>
+                            </td>
+                            <td>{repo.language}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Last Update At</strong></td>
+                            <td>{repo.update}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>License</strong></td>
+                            <td>{repo.license ? repo.license : 'N/A'}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Stars</strong></td>
+                            <td>{repo.star ? repo.star : '0'}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Opem Issue</strong></td>
+                            <td>{repo.issue ? repo.issue : '0'}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Public</strong></td>
+                            <td>{repo.isPublic ? 'Yes' : 'No'}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>URL</strong></td>
+                            <td><a herf={repo.url}>{repo.url}</a></td>
+                        </tr>
+
                 </tbody>
             </table>
         </div>
     );
 
 };
+
+export default RepoCard;
