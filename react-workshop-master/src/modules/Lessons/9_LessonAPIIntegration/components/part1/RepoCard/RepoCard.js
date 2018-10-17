ad9438d979b0f17ac303df777f91038e307bbc72
aleem <muhammad.aleem@arbisoft.com>
2018-10-17 18:48:17 +0500

    updated

--- react-workshop-master/src/modules/Lessons/9_LessonAPIIntegration/components/part1/RepoCard/RepoCard.js
+++ react-workshop-master/src/modules/Lessons/9_LessonAPIIntegration/components/part1/RepoCard/RepoCard.js
@@ -1,61 +1,57 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const RepoCard = (repo)=>{
 
-    return(
-        <div>
-            <h4><a href={repo.url} />{repo.name}</h4>
+const RepoCard = ({repo}) => {
+    return (
+        <div className="card repo">
+            <h4 className="repo-title"><a href={repo.html_url}>{repo.name}</a></h4>
             <table>
                 <tbody>
-                    <tr>
-                        <td>
-                            <strong> Description </strong>
-                        </td>
-                        <td>
-                            {repo.description ? repo.description : 'N/A'}
-                        </td>
-                    </tr>
-                        <tr>
-                            <strong><td>Defult Branch</td></strong>
-                            <td>{repo.branch}</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <strong>Language</strong>
-                            </td>
-                            <td>{repo.language}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Last Update At</strong></td>
-                            <td>{repo.update}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>License</strong></td>
-                            <td>{repo.license ? repo.license : 'N/A'}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Stars</strong></td>
-                            <td>{repo.star ? repo.star : '0'}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Opem Issue</strong></td>
-                            <td>{repo.issue ? repo.issue : '0'}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Public</strong></td>
-                            <td>{repo.isPublic ? 'Yes' : 'No'}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>URL</strong></td>
-                            <td><a herf={repo.url}>{repo.url}</a></td>
-                        </tr>
-
+                <tr>
+                    <td><strong>Description</strong></td>
+                    <td>{repo.description ? repo.description : 'N/A'}</td>
+                </tr>
+                <tr>
+                    <td><strong>Default Branch</strong></td>
+                    <td><code>{repo.default_branch}</code></td>
+                </tr>
+                <tr>
+                    <td><strong>Language</strong></td>
+                    <td>{repo.language ? repo.language : 'N/A'}</td>
+                </tr>
+                <tr>
+                    <td><strong>Last Updated at</strong></td>
+                    <td>{repo.updated_at}</td>
+                </tr>
+                <tr>
+                    <td><strong>License</strong></td>
+                    <td>{repo.license ? repo.license.name : 'N/A'}</td>
+                </tr>
+                <tr>
+                    <td><strong>Stars</strong></td>
+                    <td>{repo.stargazers_count}</td>
+                </tr>
+                <tr>
+                    <td><strong>Open Issues</strong></td>
+                    <td>{repo.open_issues_count}</td>
+                </tr>
+                <tr>
+                    <td><strong>Public</strong></td>
+                    <td>{repo.private ? 'No' : 'Yes'}</td>
+                </tr>
+                <tr>
+                    <td><strong>URL</strong></td>
+                    <td><a href={repo.html_url}>{repo.html_url}</a></td>
+                </tr>
                 </tbody>
             </table>
         </div>
     );
+};
 
+RepoCard.propTypes = {
+    repo: PropTypes.object.isRequired,
 };
 
 export default RepoCard;
