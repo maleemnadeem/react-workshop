704b5ef952fa0b92a898a95e7aee5e80222e0b15
maleemnadeem <aleemchougtai@gmail.com>
2018-10-22 22:05:52 +0500

    updated

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-
-import routes from '../../routes';
+import Navbar from '../../Navbar'
 
 import './App.css';
 
@@ -12,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
-        {routes}
+          <Navbar/>
         <Footer/>
       </div>
     );
