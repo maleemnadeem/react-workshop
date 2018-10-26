2ef366478437c66768c8fb19c20729028ff3c4ea
aleem <muhammad.aleem@arbisoft.com>
2018-10-26 10:39:26 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import {createStore} from 'redux'
+import  {Provider} from 'react-redux'
 
 import App from './modules/App/App';
+import timeReducer from './modules/Lessons/2_LessonElements/components/ReduxCompnents/Reducers/timeReducer';
 
 import './index.css';
 
+let store = createStore(timeReducer);
+
+
 ReactDOM.render((
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+
+    <Provider store = {store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
